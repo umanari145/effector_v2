db835a8687acddb0240bfcc7910cc6290d172ab9
umanari145 <umanari145@gmail.com>
2025-08-13 16:37:25 +0900

    サイト概要、会社案内を追加

--- service/app/Livewire/DynamicContents.php
+++ service/app/Livewire/DynamicContents.php
@@ -7,16 +7,17 @@ use Livewire\Component;
 class DynamicContents extends Component
 {
     public $contents;
+    public $path;
 
     public function mount()
     {
+        $this->path = request()->path();
         $this->loadContent();
     }
 
     public function loadContent()
     {
-        $path = request()->path();
-        $this->contents = $path .  "aaaa";
+        $this->contents = "aaaa";
 
         if (!$this->contents) {
             abort(404);
@@ -25,7 +26,7 @@ class DynamicContents extends Component
 
     public function render()
     {
-        return view('livewire.dynamic-contents')
+        return view('livewire.' . $this->path)
             ->extends('components.layouts.app')
             ->section('content');
     }
---
+++ service/resources/views/livewire/company.blade.php
@@ -0,0 +1,58 @@
+<div class="overflow-x-auto">
+    <div class="title">
+        <img src="{{ asset('image/company.png') }}" alt="会社案内" />
+    </div>
+    <table class="min-w-full mt-4 divide-y divide-gray-200 border border-gray-200">
+        <tbody class="bg-white divide-y divide-gray-200">
+          <tr>
+            <th class="px-6 py-2 whitespace-nowrap text-left font-medium text-gray-500 bg-green-100 w-1/4">商号</th>
+            <td class="px-6 py-2 whitespace-pre-line text-gray-900 w-3/4">光輪コーポレーション有限会社</td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 whitespace-nowrap text-left  font-medium text-gray-500 bg-green-100">本社</th>
+            <td class="px-6 py-2 whitespace-pre-line  text-gray-900">〒297-0026
+              千葉県船橋市薬円台3-16-5-606
+              TEL：080-4400-5013
+            </td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 whitespace-nowrap text-left  font-medium text-gray-500 bg-green-100">事業部</th>
+            <td class="px-6 py-2 whitespace-pre-line  text-gray-900">エフェクター蘇癒研究所</td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 whitespace-nowrap text-left  font-medium text-gray-500 bg-green-100">代表取締役</th>
+            <td class="px-6 py-2 whitespace-pre-line  text-gray-900">松本晃宜</td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 whitespace-nowrap text-left  font-medium text-gray-500 bg-green-100">会社創業</th>
+            <td class="px-6 py-2 whitespace-pre-line  text-gray-900">1983年（昭和58年）12月</td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 whitespace-nowrap text-left  font-medium text-gray-500 bg-green-100">総資本金</th>
+            <td class="px-6 py-2 whitespace-pre-line  text-gray-900">5,000,000円</td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 whitespace-nowrap text-left  font-medium text-gray-500 bg-green-100">会員数</th>
+            <td class="px-6 py-2 whitespace-pre-line  text-gray-900">8,000名</td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 text-left  font-medium text-gray-500 bg-green-100 align-top">事業内容</th>
+            <td class="px-6 py-2 whitespace-pre-line text-gray-900">1 発酵酵素・甦癒食品の開発研究及び販売
+              2 製造特許認可取得・自然健康補助食品の研究、販売
+              3 ビジネス会員及び販売代理店の募集
+            </td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 text-left  font-medium text-gray-500 bg-green-100 align-top">協賛団体</th>
+            <td class="px-6 py-2 whitespace-pre-line  text-gray-900">日本赤十字社 有功会千葉県支部
+              NPO法人 チャレンジドサポートネットJAPAN
+              全国視覚障害福祉協会
+            </td>
+          </tr>
+          <tr>
+            <th class="px-6 py-2 text-left  font-medium text-gray-500 bg-green-100 align-top">主な取引先</th>
+            <td class="px-6 py-2 whitespace-pre-line  text-gray-900">(株)バイオ・コーポレーション</td>
+          </tr>
+        </tbody>
+    </table>
+</div>

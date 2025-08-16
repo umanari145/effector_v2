2f35a3445f98632da3bb80227c18a95c67d38316
Matsu <umanari145@gmail.com>
2025-08-16 19:15:37 +0900

    migration追加

--- service/test_script.sh
+++ service/test_script.sh
@@ -4,5 +4,6 @@ apt-get install libsqlite3-dev  && apt-get install sqlite3
 cp .env.ci .env
 composer install
 touch database/database.sqlite
+php artisan db:migrate
 php artisan db:seed
 ./vendor/bin/phpunit ./tests/
